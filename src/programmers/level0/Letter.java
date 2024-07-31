1bb353c23c3c67de96384c5bfbb075a84bbab8df
SaeRyung <sr1094@naver.com>
2024-07-31 10:38:20 +0900

    programmers level0 편지

---
+++ src/programmers/level0/Letter.java
@@ -0,0 +1,13 @@
+package programmers.level0;
+
+import java.util.*;
+import java.io.*;
+
+public class Letter {
+        public int solution(String message) {
+            int letter_len = (message.length())*2;
+
+            return letter_len;
+        }
+    }
+
