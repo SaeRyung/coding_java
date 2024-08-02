2817fa413c252e8acbe99382281cd5ba86abd906
SaeRyung <101532588+SaeRyung@users.noreply.github.com>
2024-08-03 03:02:26 +0900

    Programmers level1 정수 제곱근 판별_프로그래머스 수정중

---
+++ src/programmers/level1/정수_제곱근_판별.java
@@ -0,0 +1,30 @@
+package programmers.level1;
+
+import java.util.*;
+
+public class 정수_제곱근_판별 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("임의의 양의 정수 : ");
+        int num = input.nextInt();
+
+        System.out.print("제곱 확인 정수 : ");
+        int compareNum = input.nextInt();
+
+        // 출력 넣을 변수 선언
+        int result = 0;
+
+
+        for(int i=1; i<num; i++){
+            compareNum = num/i;
+
+            if(compareNum == i){
+                result = (compareNum+1)*(compareNum+1);
+                break;
+            }else{
+                result = -1;
+            }
+        }
+        System.out.println(result);
+    }
+}
