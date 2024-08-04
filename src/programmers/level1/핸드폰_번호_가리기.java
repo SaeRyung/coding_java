d5b14b004938e2e27a107cd4929552729638dd09
SaeRyung <101532588+SaeRyung@users.noreply.github.com>
2024-08-04 21:15:33 +0900

    Programmers level1 핸드폰 번호 가리기 구현 완료

---
+++ src/programmers/level1/핸드폰_번호_가리기.java
@@ -0,0 +1,26 @@
+package programmers.level1;
+
+public class 핸드폰_번호_가리기 {
+    public String solution(String phone_number) {
+        // 폰 번호 길이
+        int len = phone_number.length();
+        // 가릴 번호 길이
+        int blind = len-4;
+
+        //공개할 마지막 4번째 자리
+        String public_num = phone_number.substring(blind, len);
+
+        String result = "";
+
+        for(int i=0; i<blind; i++){
+            // 가릴 번호 길이만큼 '*' 추가
+            result += "*";
+        }
+
+        // 가릴 번호 + 공개 4자리
+        String answer = result + public_num;
+
+        return answer;
+    }
+
+}
