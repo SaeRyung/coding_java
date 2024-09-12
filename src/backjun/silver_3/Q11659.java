6d28cd1b1157706aa3e3ddf09c96ff059865d9dc
SaeRyung <sr1094@naver.com>
2024-09-12 15:06:10 +0900

    backjun_silver3 Q11659 구간 합 구하기4 구현 완료

---
+++ src/backjun/silver_3/Q11659.java
@@ -0,0 +1,37 @@
+package backjun.silver_3;
+
+import java.util.*;
+import java.io.*;
+
+// Q11659 구간 합 구하기 4
+public class Q11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int countN = Integer.parseInt(st.nextToken()); //첫번째 N의 개수
+        int countSUM = Integer.parseInt(st.nextToken()); //합 횟수
+
+        long[] arr = new long[countN + 1]; // 합 배열
+
+        st = new StringTokenizer(br.readLine()); //배열 받아오기
+
+        // 1. 먼저 합 배열 구하기
+        for(int i=1; i<=countN; i++){
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        // 2. 합 횟수만큼 반복하기
+        for(int i=0; i<countSUM; i++){
+            // 계산 범위 받기
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            // 구간 합 출력하기
+            System.out.println(arr[B] - arr[A-1]);
+        }
+
+    }
+}
