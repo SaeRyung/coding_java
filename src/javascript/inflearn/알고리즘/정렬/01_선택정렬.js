bec8f64bcfaf9c84e6c19623f6bcedc9b062f80d
SaeRyung <sr1094@naver.com>
2024-10-14 03:47:36 +0900

    알고리즘-정렬-1. 선택정렬

---
+++ src/javascript/inflearn/알고리즘/정렬/01_선택정렬.js
@@ -0,0 +1,13 @@
+// 선택정렬
+let a = [10, 11, 9, 2, 3, 6, 5];
+let result = [];
+
+// pop 뒤에서 빼기
+// push 뒤에서 넣기
+// shift 앞에서 꺼내기
+// unshift 앞에 넣기
+for(var i=0; i<a.length; i++){
+    result.push(Math.min.apply(null, a)); //최소값 넣기
+    a.splice(a.indexOf(Math.min.apply(null, a)), 1); //넣은 인덱스 빼기
+}
+console.log(result);
